3d040dad6fac9c4f70ea079b2565fdeb53efcf98
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-05-07 20:20:08 +0100

    version 0.4.0

--- common.go
+++ common.go
@@ -1,5 +1,11 @@
 package common
 
 func GetVersion() string {
-	return "v0.3.1"
+	return "v0.4.0"
+}
+
+type Functionality struct{}
+
+func NewFunctionality() Functionality {
+	return Functionality{}
 }
